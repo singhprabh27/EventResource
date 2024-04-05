3b14517da6e833486e4273d17d06ec8c3f377dc1
Prabhjot <singhprabh2738@gmail.com>
2024-04-04 22:41:51 -0700

    run build

--- src/index.js
+++ src/index.js
@@ -8,5 +8,7 @@ import 'tachyons';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
+
+
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
